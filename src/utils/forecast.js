43138b2071231c17efab3e3f1d279c42f2ed7844
yayang876 <peter.paul.c.mercado@accenture.com>
2022-01-29 10:40:38 +0800

    Update forecast return string

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude , callback) => {
          }  else if (body.error){
               callback('Unable to find location, Try another search', undefined)
          } else {
-              callback(undefined, body.current.weather_descriptions + '. It is currently ' + body.current.temperature + ' degrees out. There is a ' + body.current.feelslike + ' degrees chance of rain');              
+              callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + "%.")              
          }
     })
 }
